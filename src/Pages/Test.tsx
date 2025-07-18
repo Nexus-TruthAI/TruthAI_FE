5eb609b16a4568ef95224804e3ff892cbb2b4b82
seoyoon04 <sykim8483@naver.com>
2025-07-18 15:00:45 +0900

    [setting] 초기 세팅

---
+++ src/Pages/Test.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    margin: 0;
+    padding: 0;
+`
+
+const Test = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+
+export default Test;
