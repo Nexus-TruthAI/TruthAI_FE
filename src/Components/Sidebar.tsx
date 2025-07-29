75b1a30f3b6eaef63daa08ad0ece128d5ddff9c6
seoyoon04 <sykim8483@naver.com>
2025-07-29 13:45:10 +0900

    [feat] sidebar구현완료, CrossCheckPage구현중

---
+++ src/Components/Sidebar.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    margin: 0 2rem 2rem 2rem;
+    padding: 0;
+    height: calc(100vh - 6rem);
+    width: 11.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    border-right: solid 1px rgba(255, 255, 255, 0.1);
+`;
+
+const PromptWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 50%;
+    width: 90%;
+    border-bottom: solid 1px rgba(255, 255, 255, 0.1);
+`;
+
+const TitleText = styled.div`
+    color: #EFEFEF;
+    font-size: 12px;
+    font-weight: 500;
+    margin: 2rem 0;
+    text-align: left;
+`;
+
+const PromptListContainer = styled.div`
+    flex: 1;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+const PromptList = styled.div`
+    width: 100%;
+`;
+
+const PromptItem = styled.div`
+    color: #fff;
+    font-size: 14px;
+    margin-bottom: 0.6rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: left;
+    width: 100%;
+`;
+
+const NewBtn = styled.div`
+    color: #C2CCFD;
+    font-size: 12px;
+    font-weight: 500;
+    align-self: flex-end;
+    padding: 0.5rem 0;
+    cursor: pointer;
+`;
+
+const CrossChecktWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 50%;
+    width: 90%;
+`;
+
+
+const Sidebar = () => {
+    const prompt_text = "새로운 프롬프트 생성하기 >";
+    const cross_text = "새로 AI 교차검증하기 >";
+
+    const prompts = Array(5).fill("최근 보고서 요약 요청").map((text, i) => `${text} ${i + 1}`);
+
+    return (
+        <Wrapper>
+            <PromptWrapper>
+                <TitleText>내 프롬프트</TitleText>
+                <PromptListContainer>
+                    <PromptList>
+                        {prompts.map((item, idx) => (
+                            <PromptItem key={idx}>{item}</PromptItem>
+                        ))}
+                    </PromptList>
+                </PromptListContainer>
+                <NewBtn>{prompt_text}</NewBtn>
+            </PromptWrapper>
+            <CrossChecktWrapper>
+                <TitleText>교차검증</TitleText>
+                <PromptListContainer>
+                    <PromptList>
+                        {prompts.map((item, idx) => (
+                            <PromptItem key={idx}>{item}</PromptItem>
+                        ))}
+                    </PromptList>
+                </PromptListContainer>
+                <NewBtn>{cross_text}</NewBtn>
+            </CrossChecktWrapper>
+        </Wrapper>
+    );
+};
+
+export default Sidebar;
